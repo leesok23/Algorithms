5253f44eaf5991de128ec12b5d1dd412da3d22b0
leesok23 <80684411+leesok23@users.noreply.github.com>
2023-06-30 15:11:29 -0700

    Create 1470. Shuffle the Array.py

---
+++ Python/LeetCode/Easy/1470. Shuffle the Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        result = []
+        for i in range(len(nums)-n):
+            result += [nums[i],nums[i+n]]
+            # result.extend([nums[i],nums[i+n]])
+        return result
