4ccf2df24c515d1ea98148bb2b3654642696c7f1
leesok23 <80684411+leesok23@users.noreply.github.com>
2023-06-30 14:28:57 -0700

    Create 278. First Bad Version.py

---
+++ Python/LeetCode/Easy/278. First Bad Version.py
@@ -0,0 +1,13 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+        while left <= right:
+            middle = (left+right) // 2
+            if isBadVersion(middle):
+                right = middle - 1
+            else:
+                left = middle + 1
+        return left
