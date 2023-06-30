8e7577725913f55d8034e3bf2657ce0656e4cca9
leesok23 <80684411+leesok23@users.noreply.github.com>
2023-06-30 14:23:51 -0700

    Update 163. Missing Ranges.py

--- Python/LeetCode/Easy/163. Missing Ranges.py
+++ Python/LeetCode/Easy/163. Missing Ranges.py
@@ -1,20 +1,25 @@
-class Solution:
-    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
-        begin = lower
-        result = []
-
+class Solution(object):
+    def findMissingRanges(self, nums, lower, upper):
+        """
+        :type nums: List[int]
+        :type lower: int
+        :type upper: int
+        :rtype: List[str]
+        """
         def addRange(left, right):
             if left == right-1:
-                result.append(str(left))
+                ranges.append(str(left))
             else:
-                result.append("{}->{}".format(left,right-1))
-        
+                ranges.append("{}->{}".format(left,right-1))
+
+        ranges = []
+        begin = lower
         for num in nums:
             if begin < num:
-                addRange(begin,num)
+                addRange(begin, num)
             begin = num + 1
         
         if begin <= upper:
-            addRange(begin,upper+1)
+            addRange(begin, upper+1)
         
-        return result
+        return ranges
