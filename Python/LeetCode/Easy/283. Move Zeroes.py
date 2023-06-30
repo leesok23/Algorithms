4fd3102f7f7225a32fbbbd7977a76b81a3f14fc2
leesok23 <80684411+leesok23@users.noreply.github.com>
2023-06-30 14:29:31 -0700

    Create 283. Move Zeroes.py

---
+++ Python/LeetCode/Easy/283. Move Zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero_pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[zero_pos] = nums[zero_pos], nums[i]
+                zero_pos += 1
+        
+        return nums
