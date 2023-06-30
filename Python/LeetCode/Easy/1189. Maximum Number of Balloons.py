154b29ba0b8f27a63cee9e8346aae4a1d6c55b4a
leesok23 <80684411+leesok23@users.noreply.github.com>
2023-06-30 14:57:37 -0700

    Create 1189. Maximum Number of Balloons.py

---
+++ Python/LeetCode/Easy/1189. Maximum Number of Balloons.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        b = text.count('b')
+        a = text.count('a')
+        l = text.count('l')
+        o = text.count('o')
+        n = text.count('n')
+        return min(b,a,l//2,o//2,n)
