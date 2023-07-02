a96454ec92d82831f7270001f35044db26ff41a8
leesok23 <80684411+leesok23@users.noreply.github.com>
2023-07-02 12:55:26 -0700

    Update 기사단원의 무기.py

--- 프로그래머스/Lv. 1/기사단원의 무기.py
+++ 프로그래머스/Lv. 1/기사단원의 무기.py
@@ -1,3 +1,4 @@
+# Version 1
 def solution(number, limit, power):
     iron = []
     for num in range(1,number+1):
@@ -12,3 +13,21 @@ def solution(number, limit, power):
         else:
             iron.append(power)
     return sum(iron)
+
+
+# Version 2
+def solution(number, limit, power):
+    answer = []
+    for num in range(1,number+1):
+        count = 0
+        for i in range(1,int(num**0.5)+1):
+            if num % i == 0:
+                if i**2 != num:
+                    count += 2
+                else:
+                    count += 1
+        if count > limit:
+            answer.append(power)
+        else:
+            answer.append(count)
+    return sum(answer)
