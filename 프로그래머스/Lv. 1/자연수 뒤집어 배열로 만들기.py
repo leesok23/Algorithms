4e719cd802f4be9ab6993d1de8366e8efde9111a
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-04-14 13:10:53 +0900

    Update 자연수 뒤집어 배열로 만들기.py

--- 프로그래머스/Lv. 1/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/Lv. 1/자연수 뒤집어 배열로 만들기.py
@@ -7,3 +7,12 @@ def solution(n):
 # Version 2
 def solution(n):
     return [int(num) for num in str(n)][::-1]
+
+
+# Version 3
+def solution(n):
+    answer = []
+    while n > 0:
+        answer.append(n%10)
+        n //= 10
+    return answer
