621ed58739909ad9fbbeb76b806c33a1911f687e
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-04-14 13:04:08 +0900

    Update 자릿수 더하기.py

--- 프로그래머스/Lv. 1/자릿수 더하기.py
+++ 프로그래머스/Lv. 1/자릿수 더하기.py
@@ -1,2 +1,12 @@
+# Version 1
 def solution(n):
     return sum([int(num) for num in str(n)])
+
+
+# Version 2
+def solution(n):
+    answer = 0
+    while n > 0:
+        answer += n % 10
+        n //= 10
+    return answer
