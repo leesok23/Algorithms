0e639695cf6c22740c6d5df0acd5371fa319c503
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-04-14 12:49:26 +0900

    Update 약수의 합.py

--- 프로그래머스/Lv. 1/약수의 합.py
+++ 프로그래머스/Lv. 1/약수의 합.py
@@ -1,3 +1,16 @@
+-- Version 1
+def solution(n):
+    answer = 0
+    num = 1
+    while num < (n**0.5):
+        if n % num == 0:
+            answer += num + n//num
+        num += 1
+    if num**2 == n:
+        answer += num
+    return answer
+    
+-- Version 2
 def solution(n):
     answer = 0
     for i in range(1,int(n**0.5)+1):
