8ed6c1189fc5dd870f1b95a49c2aaeaf8735afe0
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-04-15 12:59:54 +0900

    Update x만큼 간격이 있는 n개의 숫자.py

--- 프로그래머스/Lv. 1/x만큼 간격이 있는 n개의 숫자.py
+++ 프로그래머스/Lv. 1/x만큼 간격이 있는 n개의 숫자.py
@@ -1,2 +1,13 @@
+# Version 1
 def solution(x, n):
     return [i*x for i in range(1,n+1)]
+
+
+# Version 2
+def solution(x, n):
+    answer = [x]
+    i = 1
+    while i < n:
+        answer.append(answer[-1]+x)
+        i += 1
+    return answer
