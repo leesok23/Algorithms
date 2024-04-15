bb21be7d8c48a71e39adcf24652d4b00146d97c0
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-04-15 12:48:23 +0900

    Update 하샤드 수.py

--- 프로그래머스/Lv. 1/하샤드 수.py
+++ 프로그래머스/Lv. 1/하샤드 수.py
@@ -1,6 +1,13 @@
+# Version 1
 def solution(x):
     x_list = list(str(x))
     count = 0
     for num in x_list:
         count += int(num)
     return True if x%count==0 else False
+
+
+# Version 2
+def solution(x):
+    sum_x = sum([int(num) for num in str(x)])
+    return True if x%sum_x==0 else False
