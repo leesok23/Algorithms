8192eb44c1872abbfe5ca502cdb9bfa3d647b0a4
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-04-15 13:04:39 +0900

    Update 나머지가 1이 되는 수 찾기.py

--- 프로그래머스/Lv. 1/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/Lv. 1/나머지가 1이 되는 수 찾기.py
@@ -1,4 +1,14 @@
+# Version 1
 def solution(n):
     for x in range(2, n):
         if n % x == 1:
             return x
+
+
+# Version 2
+def solution(n):
+    answer = 2
+    while answer < n:
+        if n % answer == 1:
+            return answer
+        answer += 1
