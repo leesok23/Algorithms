175aade55ef7914353c89c021190824134c7077e
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-04-26 18:01:13 +0900

    Update 콜라츠 추측.py

--- 프로그래머스/Lv. 1/콜라츠 추측.py
+++ 프로그래머스/Lv. 1/콜라츠 추측.py
@@ -1,3 +1,17 @@
+# Version 1
+def solution(num):
+    answer = 0
+    while num > 1 and answer <= 500:
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num = num*3 + 1
+        answer += 1
+
+    return answer if answer <= 500 else -1
+
+
+# Version 2
 def solution(num):
     n = 0
     while n < 500:
