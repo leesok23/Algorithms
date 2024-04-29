8570946063233b24ba0c821d4f22f9cc7c937168
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-04-29 17:27:17 +0900

    Update 약수의 개수와 덧셈.py

--- 프로그래머스/Lv. 1/약수의 개수와 덧셈.py
+++ 프로그래머스/Lv. 1/약수의 개수와 덧셈.py
@@ -2,7 +2,8 @@
 def solution(left, right):
     answer = 0
     for i in range(left,right+1):
-        if int(i**0.5)==i**0.5:
+        if num**0.5 % 1 == 0:
+        # if int(i**0.5)==i**0.5:
             answer -= i
         else:
             answer += i
