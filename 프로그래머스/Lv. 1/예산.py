a28dfeb8582ed411af447813eaf8d73f0b1594ed
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-05-03 10:55:21 +0900

    Update 예산.py

--- 프로그래머스/Lv. 1/예산.py
+++ 프로그래머스/Lv. 1/예산.py
@@ -1,3 +1,4 @@
+# Version 1
 def solution(d, budget):
     d.sort()
     cum = 0
@@ -5,4 +6,20 @@ def solution(d, budget):
         cum += money
         if cum > budget:
             return i
+    return len(d)
+
+
+# Version 2
+def solution(d, budget):
+    d.sort()
+    for i in range(len(d),-1,-1):
+        if sum(d[:i]) <= budget:
+            return i
+
+
+# Version 3
+def solution(d, budget):
+    d.sort()
+    while sum(d) > budget:
+        d.pop()
     return len(d)
