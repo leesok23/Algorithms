89c4130da37bcd1dd41418feb6657b69711b921d
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-05-07 13:25:31 +0900

    Update 두 개 뽑아서 더하기.py

--- 프로그래머스/Lv. 1/두 개 뽑아서 더하기.py
+++ 프로그래머스/Lv. 1/두 개 뽑아서 더하기.py
@@ -1,3 +1,4 @@
+# Version 1
 def solution(numbers):
     answer = []
     for i in range(len(numbers)-1):
@@ -5,3 +6,14 @@ def solution(numbers):
             answer.append(numbers[i]+numbers[j])
             
     return sorted(list(set(answer)))
+
+
+# Version 2
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)-1):
+        for j in range(i+1,len(numbers)):
+            two_numbers = numbers[i]+numbers[j]
+            if two_numbers not in answer:
+                answer.append(two_numbers)
+    return sorted(answer)
