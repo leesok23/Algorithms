c14d0a9f233db3456360dba7eef86cfc82e95aae
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-05-07 12:58:25 +0900

    Update K번째수.py

--- 프로그래머스/Lv. 1/K번째수.py
+++ 프로그래머스/Lv. 1/K번째수.py
@@ -2,7 +2,6 @@ def solution(array, commands):
     answer = []
     for command in commands:
         i, j, k = command
-        temp = sorted(array[i-1:j])
-        answer.append(temp[k-1])
-
+        subarray = sorted(array[i-1:j])
+        answer.append(subarray[k-1])
     return answer
