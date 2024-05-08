efebbb890a55705dc6aed1cc9a892053fd4af8ee
Leesok Kim <80684411+leesok23@users.noreply.github.com>
2024-05-08 16:28:15 +0900

    Update 추억 점수.py

--- 프로그래머스/Lv. 1/추억 점수.py
+++ 프로그래머스/Lv. 1/추억 점수.py
@@ -1,10 +1,21 @@
+# Version 1
 def solution(name, yearning, photo):
     name_yearning = {x: y for x, y in zip(name, yearning)}
+    # name_yearning = dict(zip(name,yearning))
     answer = []
-    for p in photo:
+    for ph in photo:
         count = 0
-        for i in range(len(p)):
-            if p[i] in name_yearning:
-                count += name_yearning[p[i]]
+        for p in ph:
+            if p in name_yearning:
+                count += name_yearning[p]
         answer.append(count)
+    return answer
+
+
+# Version 2
+def solution(name, yearning, photo):
+    answer = []
+    name_yearning = dict(zip(name,yearning))
+    for ph in photo:
+        answer.append(sum(map(lambda x: name_yearning[x] if x in name_yearning else 0, ph)))
     return answer
